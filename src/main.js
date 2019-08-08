abca65db0325d3f334d54cda96796cb7fd039af1
Michael Tarleton <michael.tarleton@me.com>
2019-08-08 12:17:21 -0500

    Removed unecessary code

--- src/main.js
+++ src/main.js
@@ -1,11 +1,6 @@
 import { getMatchingLines, printLines } from './lib/lib'
 import { wordIndex, vowelCount, lineCount, lines } from './config/config'
 
-// Result:
-// ​Red is a non-match
-// Bold is a match
-// Grey are non-considered lines
-
 (function main() {
   const newLines = getMatchingLines(wordIndex, vowelCount, lineCount, lines)
   printLines(newLines)
